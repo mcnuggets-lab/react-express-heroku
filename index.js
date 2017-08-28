9ad7af390543b97b7adc73aef7fc87b9da4ef98c
junomak <junomak@hanluninfo.com>
2017-08-28 15:28:26 +0800

    Bug fixes for tictactoe

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/tictactoe/build'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
-app.get('/tictactoe', function(request, response) {
-  response.sendFile(path.join(__dirname, '/tictactoe/build/index.html'));
+app.route('/tictactoe').get(function(request, response, next) {
+    response.sendFile(path.join(__dirname, '/tictactoe/build/index.html'));
 });
 
+// app specific static files have to appear last, or they would invade the correct scope
+app.use(express.static(__dirname + '/tictactoe/build'));
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
