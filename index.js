929284a0654daca66c41dfb09afe9cbe5a37fade
junomak <junomak@hanluninfo.com>
2017-08-28 14:26:16 +0800

    tictactoe 2nd commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/tictactoe/build'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -15,7 +16,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/tictactoe', function(request, response) {
-  response.sendFile(path.join(__dirname+'/tictactoe/build/index.html'));
+  response.sendFile(path.join(__dirname, '/tictactoe/build/index.html'));
 });
 
 
