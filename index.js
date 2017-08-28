981c7ffb389a971a6f68423140c24d26159edaa7
junomak <junomak@hanluninfo.com>
2017-08-28 10:25:03 +0800

    First commit for tictactoe

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -12,6 +13,12 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/tictactoe', function(request, response) {
+  console.log("PATH: " + path.join(__dirname+'/tictactoe/build/index.html'));
+  response.sendFile(path.join(__dirname+'/tictactoe/build/index.html'));
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
